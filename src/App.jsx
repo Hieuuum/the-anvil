7b45d2bb74a7a03101739e41fecd51e199f38985
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-08-02 18:03:25 -0400

    Add stopwatch component

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,49 @@ import { useState, useRef } from "react";
 // import viteLogo from "/vite.svg";
 
 function App() {
-	const [timer, setTimer] = useState(0);
-	const [isStarted, setStarted] = useState(false);
+	const [startTime, setStartTime] = useState(null);
+	const [now, setNow] = useState(null);
+	const [totalTime, setTotalTime] = useState(null);
 	const intervalRef = useRef(null);
 
-	function handleClick() {
-		setStarted((prevState) => {
-			setInterval(
-				() => {
-					setTimer((prevTimer) => {
-						return prevTimer + 1;
-					});
-				},
-				prevState ? 0 : 1000
-			);
-			console.log(prevState ? 0 : 1000);
-			return prevState ? false : true;
-		});
+	function handleStart() {
+		setStartTime(Date.now());
+		setNow(Date.now());
+
+		clearInterval(intervalRef.current);
+		intervalRef.current = setInterval(() => {
+			setNow(Date.now());
+		}, 100);
+	}
+
+	function handleStop() {
+		clearInterval(intervalRef.current);
+	}
+
+	let secondsPassed = 0;
+	if (startTime != null && now != null) {
+		secondsPassed = (now - startTime) / 1000;
 	}
 
 	return (
 		<>
 			<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-				<p>Current Time: {timer}</p>
-				<button
-					onClick={handleClick}
-					className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-				>
-					Start Timer
-				</button>
+				<p>Pomodoro session: {secondsPassed.toFixed(0)}</p>
+				<p>Current Time: {secondsPassed.toFixed(0)}</p>
+				<div className="flex gap-2">
+					<button
+						onClick={handleStart}
+						className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+					>
+						Start Timer
+					</button>
+					<button
+						onClick={handleStop}
+						className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+					>
+						Stop Timer
+					</button>
+				</div>
 			</div>
 		</>
 	);
